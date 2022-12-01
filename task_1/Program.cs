3f04e74695eba83896fcfd70e150a0f68c855095
KvaskovSS <sava0980@yandex.ru>
2022-12-02 00:52:01 +0300

    Complete all tasks

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int second = Convert.ToInt32(Console.ReadLine());
+
+if(first > second){
+    Console.WriteLine($"max = {first}");
+}else if(second > first){
+    Console.WriteLine($"max = {second}");
+}
